d002959574618eccba1e3dd5cc03dbeb43ec20b6
EJCalderon <emiliojaviercalderon@gmail.com>
2022-01-03 20:12:35 -0600

    cleaning up

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const multer = require('multer')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
