d3d8a712f3eb34e0cf20929ba7ce498e723b5857
chase-risinger <ccr1988@gmail.com>
2022-01-03 19:36:10 -0600

    signup works, needed await

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-const searchRoutes = require('./search-routes.js')
+
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/results', searchRoutes)
+
 
 module.exports = router;
